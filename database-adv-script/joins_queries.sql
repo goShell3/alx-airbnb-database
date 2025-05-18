d59f05b8850ee380bfdb2a0bea404518aff0ae3a
goShell3 <herkodandy@gmail.com>
2025-05-18 22:09:27 +0300

    chore(join_scripts): updated left join using ORDER BY

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,7 +6,8 @@ INNER JOIN Users u ON b.user_id = u.user_id;
 -- Query 2: LEFT JOIN to retrieve all properties and their reviews, including properties with no reviews
 SELECT p.*, r.*
 FROM Properties p
-LEFT JOIN Reviews r ON p.property_id = r.property_id;
+LEFT JOIN Reviews r ON p.property_id = r.property_id
+ORDER BY p.price_per_night DESC, r.rating DESC NULLS LAST;
 
 -- Query 3: MySQL doesn't support FULL OUTER JOIN directly, so we emulate it with UNION of LEFT and RIGHT joins
 SELECT u.*, b.*
